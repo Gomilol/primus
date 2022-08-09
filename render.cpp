71702d316253843ef186fe5a2661c04cbd70ae7c
Chaz <70411001+Gomilol@users.noreply.github.com>
2022-08-09 15:32:06 -0500

    Changed name font
    Made name font not have a drop shadow, looks stupid.

--- render.cpp
+++ render.cpp
@@ -14,7 +14,7 @@ namespace render {
 void render::init() {
 	menu = Font(XOR("Tahoma"), 12, FW_NORMAL, FONTFLAG_NONE);
 	menu_shade = Font(XOR("Tahoma"), 12, FW_NORMAL, FONTFLAG_DROPSHADOW);
-	esp = Font(XOR("Verdana"), 12, FW_NORMAL, FONTFLAG_DROPSHADOW | FONTFLAG_ANTIALIAS);
+	esp = Font(XOR("Verdana"), 12, FW_NORMAL, FONTFLAG_ANTIALIAS);
 	esp_small = Font(XOR("Small Fonts"), 8, FW_NORMAL, FONTFLAG_OUTLINE);
 	hud = Font(XOR("Tahoma"), 16, FW_NORMAL, FONTFLAG_ANTIALIAS);
 	hudster = Font(XOR("Verdana"), 12, FW_BLACK, FONTFLAG_ANTIALIAS);
